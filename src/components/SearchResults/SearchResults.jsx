e9771ecbb04f85360198baafa619d5b41f7266a6
Ben Bauer <ben.bauer1@gmail.com>
2021-04-24 14:08:16 -0500

    modal working for search clicks

--- src/components/SearchResults/SearchResults.jsx
+++ src/components/SearchResults/SearchResults.jsx
@@ -1,11 +1,62 @@
 import './SearchResults.css'
-import { useDispatch, useSelector } from "react-redux";
-import { useHistory } from 'react-router';
+import {useSelector } from "react-redux";
+import React, { useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Modal from '@material-ui/core/Modal';
 
 
 function SearchResults () {
     const searchlisting = useSelector((store) => store.searchlisting);
     const user = useSelector((store => store.user))
+
+//MODAL CODE
+function getModalStyle() {
+    const top = 45;
+    const left = 45;
+  
+    return {
+      top: `${top}%`,
+      left: `${left}%`,
+      transform: `translate(-${top}%, -${left}%)`,
+    };
+  }
+
+  const useStyles = makeStyles((theme) => ({
+    paper: {
+      position: 'absolute',
+      width: 200,
+      backgroundColor: theme.palette.background.paper,
+      border: '2px solid #000',
+      boxShadow: theme.shadows[5],
+      padding: theme.spacing(2, 4, 3),
+    },
+  }));
+
+  const classes = useStyles();
+  // getModalStyle is not a pure function, we roll the style only on the first render
+  const [modalStyle] = React.useState(getModalStyle);
+  const [open, setOpen] = React.useState(false);
+  const [item, setItem] = useState('')
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+//END MODAL CODE
+
+  const body = (
+    <div style={modalStyle} className={classes.paper}>
+      <h4 id="simple-modal-title">Item info</h4>
+      <p id="simple-modal-description">
+        Item: {item}
+      </p>
+    </div>
+  );
+
 
     const forSale = (listing) =>{
         if(listing.for_sale == true){
@@ -33,11 +84,12 @@ function SearchResults () {
             )
             } 
     }
-    const dispatch = useDispatch()
-    const history = useHistory()
+
     const resultClicked = (listingID) => {
-        dispatch({ type: 'FETCH_LISTING', payload: listingID})
-        history.push('/listinginfo')
+        console.log(listingID)
+        console.log(searchlisting[listingID])
+        setItem(searchlisting[listingID].vegetable)
+        handleOpen()
     }
 
     const checkLoad = () =>{
@@ -48,11 +100,19 @@ function SearchResults () {
         } else{
             return(
                 <> 
+                    <Modal
+                        open={open}
+                        onClose={handleClose}
+                        aria-labelledby="simple-modal-title"
+                        aria-describedby="simple-modal-description"
+                    >
+                        {body}
+                    </Modal>
                 <div className="searchResultsMain">
                     <div className="searchResults">
-                    {searchlisting.map(listing => {
+                    {searchlisting.map((listing, index) => {
                             return(
-                                <div className="searchListingResult" onClick={() => {resultClicked(listing.id)}}>
+                                <div className="searchListingResult" onClick={() => {resultClicked(index)}}>
                                     <div className="itemInfo">
                                         <h4>{listing.vegetable}</h4>
                                         <h4>{JSON.stringify(listing)}</h4>
